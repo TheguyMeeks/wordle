215c714009ef55d086366238c20a9f868b03fa62
Meeks <meekd2k@gmail.com>
2026-03-18 01:36:13 -0600

    set up some classes

---
+++ src/LetterResult.java
@@ -0,0 +1,7 @@
+public enum LetterResult {
+    CORRECT,  // right letter, right place (green)
+    PRESENT, // right letter wrong place
+    ABSENT; // letter not in the word at all (gray)
+
+
+}
---
+++ src/WordList.java
@@ -0,0 +1,8 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class WordList {
+
+}
