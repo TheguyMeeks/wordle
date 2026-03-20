0cd2826867740498b0961401e69c6d00acda85bf
Meeks <meekd2k@gmail.com>
2026-03-20 00:28:36 -0600

    finished loadWords() and getRandomWord();

--- src/WordList.java
+++ src/WordList.java
@@ -1,31 +1,53 @@
+import java.io.File;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
 
 
 public class WordList {
-    private List<String> words; // private so nothing touches it outside the class
+    private List<String> wordBank; // private so nothing touches it outside the class
 
     public WordList(String filepath) { // constructor takes a file path so it knows where to load words from.
-        this.words =  new ArrayList<>(); // initializes this objs list as empty, to be filled when we load words
-        // need a loadWords() method that takes in filepath, better to have a separate method for this; a function should do one specific thing
+        this.wordBank =  new ArrayList<>(); // initializes this objs list as empty, to be filled when we load words
+        loadWords(filepath); // call load words to fill our words
     }
 
     private void loadWords(String filepath) {
         // TODO
-        //reads file and fills this.words
+        File file = new File(filepath);
+        try {
+            Scanner reader = new Scanner(file);
+
+            // read through the file and add words to the word bank
+            while (reader.hasNextLine()) {
+                String currentLine = reader.nextLine();
+                String[] parts = currentLine.split(",");
+
+                // loop to cleanly move the contents of parts into this.wordBank
+                for (String word : parts) {
+                    word = word.trim().toLowerCase();
+                    this.wordBank.add(word);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getRandomWord() {
         // TODO
-        // get random word from this.words
-        return null;
+        Random randomizer = new Random();
+        int index = randomizer.nextInt(wordBank.size()); // picks a random num between 0 and the size of the wordBank list. and stores it in index
+        return wordBank.get(index); // returns the word at that index in wordBank
     }
 
     public boolean isValidWord(String guess) {
         String formattedGuess = guess.toLowerCase();
-        return words.contains(formattedGuess);
+        return wordBank.contains(formattedGuess);
     }
 
 }
