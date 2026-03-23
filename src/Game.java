5df04cd0395a308b304ff3465eb8994099a584c9
Meeks <meekd2k@gmail.com>
2026-03-22 22:29:05 -0600

    set up Game.java's skeleton

--- src/Game.java
+++ src/Game.java
@@ -12,4 +12,27 @@ public class Game {
         this.guesses = new ArrayList<>(); // empty. will fill as the player plays
         this.maxAttempts = 6;
     }
+
+    // we return an array (of size 5) because we are evaluating each letter in the word and it's position
+    public LetterResult[] submitGuess(String guess) {
+        guesses.add(guess); // add a guess to the list
+        return evaluateGuess(guess); // evaluate said guess
+    }
+
+    public LetterResult[] evaluateGuess(String guess) {
+        // evaluate the guess that was just submitted
+    }
+
+    public boolean isWon() {
+        if (guesses.isEmpty()) {
+            return false; // if the player has not guessed the game is not over. duh
+        }
+
+        int mostRecent = guesses.size() - 1; // index of the most recent guess
+        return guesses.get(mostRecent).equals(secretWord);
+    }
+
+    public boolean isOver() {
+        return isWon() || guesses.size() >= maxAttempts;
+    }
 }
