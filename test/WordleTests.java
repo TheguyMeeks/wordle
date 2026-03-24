f6824d1c29c8bfc8db8a69f980e9cb896285ce46
Meeks <meekd2k@gmail.com>
2026-03-23 20:27:14 -0600

    evauluateGuess() first test, evaluateGuessCorrect() simulates what happens when the guess is the word

--- test/WordleTests.java
+++ test/WordleTests.java
@@ -1,9 +1,16 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WordleTests {
     @Test
     void evaluateGuessCorrect() {
+        // make the guess the exact same with the secret word
         Game game = new Game("Spike");
+        LetterResult[] result = game.submitGuess("Spike"); // runs evaluateGuess through submitGuess first, which first deals with capitalization
+
+        LetterResult[] expected = {LetterResult.CORRECT,LetterResult.CORRECT,LetterResult.CORRECT,LetterResult.CORRECT,LetterResult.CORRECT};
+
+        assertArrayEquals(result,expected);
     }
 
 }
